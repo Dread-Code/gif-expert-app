9981b0f56d294c7e10d0bac091c34a3a2d740180
DreadfulCrazy <lucasospina97@gmail.com>
2021-07-02 23:39:44 -0500

    Grid and some styles changed

--- src/components/GiftExpertApp/GiftExpertApp.jsx
+++ src/components/GiftExpertApp/GiftExpertApp.jsx
@@ -1,18 +1,21 @@
 import React, { useState } from 'react'
 import AddCategory from '../AddCategory'
 import GifGrid from '../GifGrid'
+import './GiftExpertApp.css'
 
 const GiftExpertApp = () => {
-  const [categories, setCategories] = useState(['Dororo'])
+  const [categories, setCategories] = useState(['bleach'])
 
   return (
     <div>
       <h2>GiftExpertApp</h2>
       <AddCategory setCategories={setCategories} categories={categories} />
       <hr></hr>
-      {categories.map((category, key) => (
-        <GifGrid category={category} key={category + key} />
-      ))}
+      <div className="gif-grid">
+        {categories.map((category, key) => (
+          <GifGrid category={category} key={category + key} />
+        ))}
+      </div>
     </div>
   )
 }
