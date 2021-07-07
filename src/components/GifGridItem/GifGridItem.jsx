e05edea863179f902f47cc513aec7e9afe05acb7
DreadfulCrazy <lucasospina97@gmail.com>
2021-07-06 21:53:33 -0500

    Tests for GifGridItem

--- src/components/GifGridItem/GifGridItem.jsx
+++ src/components/GifGridItem/GifGridItem.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import './GifGridItem.css'
+import PropTypes from 'prop-types'
 
 const GifGridItem = ({ title, url, height, width }) => {
   return (
-    <div className="card animate__animated animate__fadeIn" style={{ backgroundImage: `url("${url}")` }} />
+    <div
+      className="card animate__animated animate__fadeIn"
+      style={{ backgroundImage: `url("${url}")` }}
+      alt={title}
+    />
   )
+}
+
+GifGridItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired
 }
 
 export default GifGridItem
--- src/components/GifGridItem/GifGridItem.test.jsx
+++ src/components/GifGridItem/GifGridItem.test.jsx
@@ -2,9 +2,27 @@ import { shallow } from 'enzyme'
 import GifGridItem from './GifGridItem'
 
 describe('Gif Grid Item', () => {
-  test('should Show shallow', () => {
-    const wrapper = shallow(<GifGridItem />)
+  const title = 'Un titulo'
+  const url = 'https://localhost/algo.jpg'
+  const wrapper = shallow(<GifGridItem title={title} url={url} />)
 
+  test('should Show shallow', () => {
     expect(wrapper).toMatchSnapshot()
+  })
+
+  test('should have a equal alt', () => {
+    const div = wrapper.find('div')
+    expect(div.prop('alt')).toBe(title)
+  })
+
+  test('should have a equal url', () => {
+    const div = wrapper.find('div')
+    expect(div.prop('style').backgroundImage).toBe(`url("${url}")`)
+  })
+
+  test('should have animate__fadeIn', () => {
+    const div = wrapper.find('div')
+    const className = div.prop('className')
+    expect(className.includes('animate__fadeIn')).toBe(true)
   })
 })
